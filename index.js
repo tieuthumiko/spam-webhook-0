674914a3e512c2ebe8c55aca49839914a66cfed5
tieuthumiko <nguyenquangnhatminh0101121791@gmail.com>
2026-03-16 17:24:58 +0700

    update

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
 const { MessageEmbed, WebhookClient } = require('discord.js');
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 const embed = new MessageEmbed()
     .setColor('#555555')
@@ -12,3 +16,13 @@ const webhookurl = new WebhookClient({ id: process.env.WEBHOOK_ID, token: proces
 setInterval(() => {
     webhookurl.send({embeds: [ embed ]});
   }, 1);
+
+app.get("/", (req,res)=>{
+    res.send("Bot is alive");
+});
+
+app.listen(PORT, ()=>{
+    console.log("Server running on port " + PORT);
+});
+
+  process.on('uncaughtException', console.error);
